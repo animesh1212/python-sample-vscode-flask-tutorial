2f46915eb47dffc750b1604712f491727fdbb27c
Viswanatha Srinivas <flicke>
2020-09-11 20:52:17 +0530

    change app response hello world

--- startup.py
+++ startup.py
@@ -8,5 +8,13 @@ The solution is to provide a simple alternate startup file, like this present
 startup.py, that just imports the app object. You can then just specify
 startup:app in the Gunicorn command.
 """
+from flask import Flask
+app = Flask(__name__)
 
-from hello_app.webapp import app
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",threaded=True)
